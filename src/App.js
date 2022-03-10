5b77fe0bee0fef3740656a47433a419c0ce2ec15
Seu nome <seuemail@exemplo.br>
2022-03-10 16:14:12 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as api from './services/api';
 
 function App() {
+
+  api.getProductsFromCategoryAndQuery().then(query => { console.log(query) });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>Edit src/App.js and save to reload.</p>
-        <p>oi</p>
         <a
           className="App-link"
           href="https://reactjs.org"
