b50bb2a050658a91afb6717193dcbe68b16bfe5a
Seu nome <seuemail@exemplo.br>
2022-03-15 00:52:38 -0300

    main-group-18-requisito-07-concluido

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Produtos extends React.Component {
   render() {
     const { listaProdutos } = this.props;
+    console.log(listaProdutos);
     return (
       listaProdutos.map((produto, i) => (
         <div key={ i } data-testid="product">
           <p>{ produto.title }</p>
           <img src={ produto.thumbnail } alt="foto produto" />
           <p>{ produto.price }</p>
+          <Link to={ `/detalhes/${produto.id}` }>
+            <button
+              data-testid="product-detail-link"
+              type="button"
+            >
+              Ver detalhes
+            </button>
+          </Link>
         </div>
       ))
     );
