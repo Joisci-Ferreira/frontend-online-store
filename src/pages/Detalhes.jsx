fc1bf301d389ba74674da38b99f02f331e886f95
Gabriel Coelho <coelho.tibbers@gmail.com>
2022-03-16 12:58:28 -0300

    Requisito 11 ok

--- src/pages/Detalhes.jsx
+++ src/pages/Detalhes.jsx
@@ -8,9 +8,19 @@ class Detalhes extends React.Component {
     super();
     this.state = {
       produto: {},
+      endereco: '',
+      nota: '',
+      texto: '',
+      test: [{
+        endereco: '',
+        nota: '',
+        texto: '',
+      }],
     };
     this.activeGetProductsId = this.activeGetProductsId.bind(this);
     this.ClickButton = this.ClickButton.bind(this);
+    this.theOnChange = this.theOnChange.bind(this);
+    this.ClickSubmit = this.ClickSubmit.bind(this);
   }
 
   async componentDidMount() {
@@ -34,11 +44,50 @@ class Detalhes extends React.Component {
     localStorage.setItem('product', resultJSON);
   }
 
+  theOnChange({ target }) {
+    if (target.name === 'email') {
+      this.setState({ endereco: target.value });
+    } else if (target.name === 'radio') {
+      this.setState({ nota: target.value });
+    } else if (target.name === 'text') {
+      this.setState({ texto: target.value });
+    }
+  }
+
+  ClickSubmit() {
+    const { match: { params: { id } } } = this.props;
+    const { endereco, nota, texto } = this.state;
+    const result = {
+      email: endereco,
+      avaliacao: nota,
+      comentario: texto,
+    };
+    this.setState((a) => ({ test: [...a.test, result] }));
+    const requestResult = localStorage.getItem(id);
+    const requestResultJSON = JSON.parse(requestResult);
+    const validateJSON = requestResultJSON === null ? '' : requestResultJSON;
+    const encapsulate = [...validateJSON, result];
+    const resultJSON = JSON.stringify(encapsulate);
+    console.log(resultJSON);
+    localStorage.setItem(id, resultJSON);
+  }
+
   render() {
     const { produto: { title, price, thumbnail, attributes } } = this.state;
+    const { match: { params: { id } } } = this.props;
     const result = attributes === undefined ? (<p>carregando</p>) : (attributes
-      .map((attribute, id) => (
-        <li key={ id }>{ `${attribute.name} - ${attribute.value_name}` }</li>)));
+      .map((attribute, i) => (
+        <li key={ i }>{ `${attribute.name} - ${attribute.value_name}` }</li>)));
+    const local = localStorage.getItem(id);
+    const localJSON = JSON.parse(local);
+    const validateJSON = localJSON === null ? <p>nenhuma avaliação</p> : localJSON
+      .map((e, i) => (
+        <div key={ i }>
+          <p>{ e.email }</p>
+          <p>{ e.avaliacao }</p>
+          <p>{ e.comentario }</p>
+        </div>
+      ));
     return (
       <div>
         <h2
@@ -60,6 +109,67 @@ class Detalhes extends React.Component {
         <Link to="/cart" data-testid="shopping-cart-button">
           <button type="button">carrinho</button>
         </Link>
+        <form>
+          <input
+            type="email"
+            data-testid="product-detail-email"
+            placeholder="seu email"
+            name="email"
+            onChange={ this.theOnChange }
+          />
+          <label htmlFor="radio" isRequired>
+            <input
+              type="radio"
+              data-testid="1-rating"
+              name="radio"
+              value="1"
+              onChange={ this.theOnChange }
+            />
+            <input
+              type="radio"
+              data-testid="2-rating"
+              name="radio"
+              value="2"
+              onChange={ this.theOnChange }
+            />
+            <input
+              type="radio"
+              data-testid="3-rating"
+              name="radio"
+              value="3"
+              onChange={ this.theOnChange }
+            />
+            <input
+              type="radio"
+              data-testid="4-rating"
+              name="radio"
+              value="4"
+              onChange={ this.theOnChange }
+            />
+            <input
+              type="radio"
+              data-testid="5-rating"
+              name="radio"
+              value="5"
+              onChange={ this.theOnChange }
+            />
+            <input
+              type="text"
+              data-testid="product-detail-evaluation"
+              name="text"
+              placeholder="algo sobre o produto"
+              onChange={ this.theOnChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="submit-review-btn"
+            onClick={ this.ClickSubmit }
+          >
+            Enviar
+          </button>
+        </form>
+        { validateJSON }
       </div>
     );
   }
